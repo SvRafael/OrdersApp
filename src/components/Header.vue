4a50f875d3dfdcae3e1538dac3d8aca4f6f3ba20
SvRafael <rafaelsvsouza@hotmail.com>
2024-08-24 15:57:00 -0300

    fix: ajuste header e title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,7 +17,7 @@ const handleOpenModal = function () {
 <template>
   <header :class="style.header">
     <div :class="style.headerContent">
-      <div :class="style.logo">Hamburgueria LA</div>
+      <div :class="style.logo">Controle de Caixa</div>
       <nav :class="style.nav">
         <ul :class="style.nav_list">
           <li :class="style.nav_item">
